9950567b33e1dcd1d9f6be33db1bc456e3b723ea
pgadula <pgadula@Mac-mini-pgadula.local>
2024-12-29 18:01:54 +0100

    refactor engine

--- chess_main/src/main.rs
+++ chess_main/src/main.rs
@@ -1,99 +1,12 @@
-use std::{io, time::Instant};
+use std::io;
 
-use chess_uci::{engine::Engine, lexer::Lexer};
+use chess_uci::engine::Engine;
 fn main() {
     let mut engine = Engine::new();
-
     while engine.is_running {
         let mut buf = String::from("");
         let _ = io::stdin().read_line(&mut buf);
         let chars = &buf.chars().collect::<Vec<char>>();
-        let mut lexer = Lexer::from(chars);
-        while let Some(token) = lexer.next() {
-            match token.as_str() {
-                "uci" => {
-                    println!("id name PrzemChess Chess");
-                    println!("id author Przemyslaw Gadula");
-                    println!("uciok");
-                }
-                "isready" => {
-                    println!("readyok");
-                }
-                "position" => {
-                    if let Some(next_token) = lexer.next() {
-                        match next_token.as_str() {
-                            "startpos" => {
-                                    let mut tokens = Vec::new(); // Buffer to collect tokens
-
-                                    while let Some(token) = lexer.next() {
-                                        if token == "moves" {
-                                            continue;
-                                        }
-                                        tokens.push(token);
-                                    }
-
-                                    let uci_moves: Vec<String> = tokens
-                                    .join(" ")
-                                    .split_whitespace() 
-                                    .map(|s| s.to_string()) 
-                                    .collect(); 
-
-                                    if uci_moves.len() > 0 {
-                                        engine.apply_move(uci_moves);
-                                    } else {
-                                        engine.new_game();
-                                    }
-                            }
-                            "fen" => {
-                                let mut positon = Vec::new();
-                                while let Some(position_token) = lexer.next() {
-                                    positon.push(position_token);
-                                }
-                                let fen = positon.concat();
-                                println!("fen, {}", fen);
-                                engine.from(&fen);
-                            }
-                            _ => {
-                                println!("
-                                Invalid usage of position command:
-                                  position startpos       : Sets the starting position.
-                                  position fen [FEN]      : Sets the position using a FEN string.
-                                  position startpos moves [moves] : Sets the position from the starting position and the list of moves.
-                                ");
-                            }
-                        }
-                    } else {
-                        println!("
-                        Invalid usage of positon \n 
-                            position startpos: Sets the starting position.
-                            position fen [FEN]: Sets the position using a FEN string.
-                            position startpos moves [moves]: Sets the position from the starting position and the list of moves.
-                        ")
-                    }
-                }
-                "go" => {
-                    println!("UciGo");
-                    let now = Instant::now();
-                    engine.go(Some(24));
-                    let elapsed = now.elapsed();
-                    println!("Thinking time: {:.2?}", elapsed);
-                }
-                "ucinewgame" => {
-                    engine.new_game();
-                }
-                "stop" => {
-                    engine.stop_thinking();
-                }
-                "quit" => {
-                    engine.quit();
-                }
-                "d" =>{
-                    engine.print();
-                }
-                _ => {
-                    eprintln!("Unknown command: {:?}", token);
-                }
-            }
-        }
+        engine.process_command(chars);
     }
 }
--- chess_uci/src/engine.rs
+++ chess_uci/src/engine.rs
@@ -1,23 +1,24 @@
 use std::{
     sync::{
         atomic::{AtomicBool, Ordering},
-        mpsc, Arc,
+        Arc,
     },
     thread::{self, JoinHandle},
+    time::Instant,
 };
 
 use chess_core::{bitboard::GameState, fen::FenParser, types::PieceMove};
 
-use crate::search_engine::SearchEngine;
+use crate::{lexer::Lexer, search_engine::SearchEngine};
 
 pub struct Engine {
     pub use_multithreading: bool,
     pub is_running: bool,
     pub search_engine: SearchEngine,
-    is_searching: bool,
+    pub child_handle: Option<JoinHandle<String>>,
     board: GameState,
-    child_handle: Option<JoinHandle<String>>,
     cancellation_signal: Arc<AtomicBool>,
+    start_thinking_time: Option<Instant>,
 }
 
 impl Engine {
@@ -26,23 +27,23 @@ impl Engine {
         return Engine {
             board,
             is_running: true,
-            is_searching: false,
             search_engine: SearchEngine::new(),
             use_multithreading: true,
             child_handle: None,
             cancellation_signal: Arc::new(AtomicBool::new(false)),
+            start_thinking_time: None,
         };
     }
     pub fn go(&mut self, depth: Option<u8>) {
         self.cancellation_signal.store(false, Ordering::SeqCst);
         self.search_engine.max_depth = depth.unwrap_or(24);
+        self.start_thinking_time = Some(Instant::now());
 
         let mut search = self.search_engine.clone();
         let use_multithreading = self.use_multithreading;
         let board = self.board.clone();
         let cancellation_token = self.cancellation_signal.clone();
 
-        // Spawn thread and store the handle
         self.child_handle = Some(thread::spawn(move || {
             if use_multithreading {
                 search.rayon_search(&board, cancellation_token)
@@ -50,20 +51,20 @@ impl Engine {
                 search.search(&board)
             }
         }));
-
-        // Don’t `join` here – return immediately to allow background searching
-        println!("Search started in background thread");
     }
 
     pub fn stop_thinking(&mut self) {
-        self.cancellation_signal.store(true, Ordering::SeqCst);
+        self.cancellation_signal.store(true, Ordering::Relaxed);
+        if let Some(now) = self.start_thinking_time {
+            let elapsed = now.elapsed();
+            println!("Thinking time: {:.2?}", elapsed);
+        }
+        self.start_thinking_time = None;
         self.join_search();
-        
     }
 
-    pub fn join_search(&mut self) {
+    fn join_search(&mut self) {
         // If the thread is still running, .take() the handle out of self.child
-
         if let Some(child_thread) = self.child_handle.take() {
             match child_thread.join() {
                 Ok(result) => {
@@ -99,6 +100,97 @@ impl Engine {
         for uci in uci_moves {
             let piece_move = PieceMove::from_uci(&uci, &self.board);
             self.board.make_move(&piece_move);
+        }
+    }
+
+    pub fn process_command(&mut self, chars: &Vec<char>) {
+        let mut lexer = Lexer::from(chars);
+        while let Some(token) = lexer.next() {
+            match token.as_str() {
+                "uci" => {
+                    println!("id name PrzemChess Chess");
+                    println!("id author Przemyslaw Gadula");
+                    println!("uciok");
+                }
+                "isready" => {
+                    println!("readyok");
+                }
+                "position" => {
+                    if let Some(next_token) = lexer.next() {
+                        match next_token.as_str() {
+                            "startpos" => {
+                                let mut tokens = Vec::new(); // Buffer to collect tokens
+
+                                while let Some(token) = lexer.next() {
+                                    if token == "moves" {
+                                        continue;
+                                    }
+                                    tokens.push(token);
+                                }
+
+                                let uci_moves: Vec<String> = tokens
+                                    .join(" ")
+                                    .split_whitespace()
+                                    .map(|s| s.to_string())
+                                    .collect();
+
+                                if uci_moves.len() > 0 {
+                                    self.apply_move(uci_moves);
+                                } else {
+                                    self.new_game();
+                                }
+                            }
+                            "fen" => {
+                                let mut positon = Vec::new();
+                                while let Some(position_token) = lexer.next() {
+                                    positon.push(position_token);
+                                }
+                                let fen = positon.concat();
+                                println!("fen, {}", fen);
+                                self.from(&fen);
+                            }
+                            _ => {
+                                println!("
+                                Invalid usage of position command:
+                                  position startpos       : Sets the starting position.
+                                  position fen [FEN]      : Sets the position using a FEN string.
+                                  position startpos moves [moves] : Sets the position from the starting position and the list of moves.
+                                ");
+                            }
+                        }
+                    } else {
+                        println!("
+                        Invalid usage of positon \n 
+                            position startpos: Sets the starting position.
+                            position fen [FEN]: Sets the position using a FEN string.
+                            position startpos moves [moves]: Sets the position from the starting position and the list of moves.
+                        ")
+                    }
+                }
+                "go" => {
+                    if self.child_handle.is_none() {
+                        println!("UciGo");
+                        self.go(Some(24));
+                    } else {
+                        println!("engine has started already!");
+                    }
+                }
+                "ucinewgame" => {
+                    self.new_game();
+                }
+                "stop" => {
+                    self.stop_thinking();
+                }
+                "quit" => {
+                    self.quit();
+                }
+                "d" => {
+                    self.print();
+                }
+                _ => {
+                    eprintln!("Unknown command: {:?}", token);
+                }
+            }
         }
     }
 }
